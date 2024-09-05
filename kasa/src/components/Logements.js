32eb752d12cb6ca579ab02b964acd255458bad71
MMSG97 <maria.m.mga88@gmail.com>
2024-09-05 16:44:48 +0200

    mise en place du footer + parcours de la liste des logements.json ave map() mais éléments 'indefined'...

--- kasa/src/components/Logements.js
+++ kasa/src/components/Logements.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Card from "./Cards";
-import "../datas/logements.json";
+import logts from "../datas/logements.json";
 
 
 function Logements() {
+    console.log(logts.cover)
     return (
         <div className="cardsContainer">
-            {}
+            {logts.map((logt) => {
+                return (
+                    <article key={logt.id}>
+
+                        <Card cardLink={'#'} img={logts.cover} title={logts.title} />
+                    </article>
+                )
+            })}
+
         </div>
     )
 }
