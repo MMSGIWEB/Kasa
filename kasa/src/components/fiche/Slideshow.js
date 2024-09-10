04429d1e89f005cc77fa3c503cc55295e396cb75
MMSG97 <maria.m.mga88@gmail.com>
2024-09-10 19:08:28 +0200

    mise en place du slideShow avec des fonctions attribuées aux flèches pour le changement d'img

--- kasa/src/components/fiche/Slideshow.js
+++ kasa/src/components/fiche/Slideshow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import appts from "../../datas/logements.json"
 import { useParams } from "react-router-dom";
 
@@ -8,22 +8,52 @@ function Slideshow() {
     //fait correspondre l'id à celui du logement
     const logement = appts.find((appt) => appt.id === id)
 
+    const [currentPicture, setCurrentPicture] = useState(0)
+
+    //fonction qui donne une classe à l'image actuelle
+    const getClassName = (i) => {
+        if (i === currentPicture) {
+            return "currentImg";
+            return ""
+        }
+    }
+
     //récup des images pour chaque fiche logement
     const pictures = logement?.pictures.map((picture, i) => {
         return (
-            <img key={i} src={picture} alt="image du logement" />
+            <img key={i} src={picture} alt="image du logement" className={getClassName(i)} />
         )
     })
+
+    const moveToNext= () => {
+        //currentState va changer de 1 à 1, sans dépasser la longueur du nb d'img
+        //'%..' limite la longueur de l'arrêt
+        setCurrentPicture((currentPicture + 1) % pictures.length)
+    }
+
+    const moveToPrevious= () => {
+        //notre constante équivaut l'img actuelle - 1 lorsqu'on on arrive à la fin du nb d'imgs
+        const afterPictureLength = currentPicture - 1
+        //mais si c'est inférieur à 0 on applique 
+        if (afterPictureLength < 0) {
+            //application du changement d'img avec le code en ()
+            setCurrentPicture(pictures.length - 1)
+            return
+        }
+        //sinon le code habituel pour le changement d'img
+        setCurrentPicture((currentPicture - 1) % pictures.length)
+    }
+
 
     return (
         <>
             <div className="infoBanner">
                 {pictures}
-                <button className="arrowLeft arrow">
-                    <i className="fa-solid fa-chevron-left"></i>
+                <button className="arrowLeft arrow"  >
+                    <i className="fa-solid fa-chevron-left" onClick={moveToPrevious}></i>
                 </button>
                 <button className="arrowRight arrow">
-                    <i className="fa-solid fa-chevron-right"></i>
+                    <i className="fa-solid fa-chevron-right"  onClick={moveToNext}></i>
                 </button>
             </div>
 
