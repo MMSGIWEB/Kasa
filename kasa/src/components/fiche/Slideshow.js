2a3cd3633d65ff0a270d511fdedb125e9e49c52a
MMSG97 <maria.m.mga88@gmail.com>
2024-09-11 19:15:54 +0200

    ajout de la condition de la présence des flèches le nombre d'images pour une fiche est supérieure à 1

--- kasa/src/components/fiche/Slideshow.js
+++ kasa/src/components/fiche/Slideshow.js
@@ -25,13 +25,13 @@ function Slideshow() {
         )
     })
 
-    const moveToNext= () => {
+    const moveToNext = () => {
         //currentState va changer de 1 à 1, sans dépasser la longueur du nb d'img
         //'%..' limite la longueur de l'arrêt
         setCurrentPicture((currentPicture + 1) % pictures.length)
     }
 
-    const moveToPrevious= () => {
+    const moveToPrevious = () => {
         //notre constante équivaut l'img actuelle - 1 lorsqu'on on arrive à la fin du nb d'imgs
         const afterPictureLength = currentPicture - 1
         //mais si c'est inférieur à 0 on applique 
@@ -42,6 +42,7 @@ function Slideshow() {
         }
         //sinon le code habituel pour le changement d'img
         setCurrentPicture((currentPicture - 1) % pictures.length)
+
     }
 
 
@@ -49,11 +50,16 @@ function Slideshow() {
         <>
             <div className="infoBanner">
                 {pictures}
-                <button className="arrowLeft arrow"  >
-                    <i className="fa-solid fa-chevron-left" onClick={moveToPrevious}></i>
+                {/* conditions : si le nombre d'img est supérieur à 1 alors on intègre les flèches */}
+                <button className="arrowLeft arrow">
+                    {pictures.length > 1 && (
+                        <i className="fa-solid fa-chevron-left" onClick={moveToPrevious}></i>
+                    )}
                 </button>
                 <button className="arrowRight arrow">
-                    <i className="fa-solid fa-chevron-right"  onClick={moveToNext}></i>
+                    {pictures.length > 1 && (
+                        <i className="fa-solid fa-chevron-right" onClick={moveToNext}></i>
+                    )}
                 </button>
             </div>
 
